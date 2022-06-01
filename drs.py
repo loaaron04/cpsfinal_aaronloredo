b579dabc5d578db334d6d513c0b74fbab2b66cea
loaaron04 <105502585+loaaron04@users.noreply.github.com>
2022-06-01 18:27:52 -0500

    Update drs.py

--- drs.py
+++ drs.py
@@ -1,1 +1,20 @@
+import random
 
+min = 1
+max = 6
+
+roll_again = "yes"
+
+while roll_again == "yes" or roll_again == "y":
+    print ("Rolling the dices...")
+    print ("The values are....")
+    print (random.randint(min, max))
+    print (random.randint(min, max))
+
+    restart = input("Do you want to roll the dices again?(y/n)")
+    if restart == "y" or restart == "Y":
+        game()
+
+
+if __name__ == "__main__":
+    game()
